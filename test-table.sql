f1f1cd4756a67c7c97ccabfb87f904cbbd3e82f2
Cj <castro_charljoven@plpasig.edu.ph>
2026-07-17 21:39:47 +0800

    Add View Modal in Assigned Users

--- test-table.sql
+++
@@ -1,9 +0,0 @@
--- Test script to verify user_profiles table
-SELECT 
-    column_name, 
-    data_type, 
-    is_nullable, 
-    column_default
-FROM information_schema.columns
-WHERE table_name = 'user_profiles'
-ORDER BY ordinal_position;
