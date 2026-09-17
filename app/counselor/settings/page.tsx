e8b25b26df4f76b7f94d905219751c02c9d53ac9
Cj <castro_charljoven@plpasig.edu.ph>
2026-09-18 03:40:11 +0800

    fix in counselor

--- app/counselor/settings/page.tsx
+++ app/counselor/settings/page.tsx
@@ -544,7 +544,6 @@ export default function CounselorSettingsPage() {
                   <div className="w-11 h-6 bg-light-gray peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-primary-blue/20 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-primary-blue"></div>
                 </label>
               </div>
-              </div>
 
               {/* Shift Preference */}
               <div className="p-4 bg-light-gray/30 rounded-xl space-y-3">
